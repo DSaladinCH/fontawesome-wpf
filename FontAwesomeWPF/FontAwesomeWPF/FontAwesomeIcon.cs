eb683fb768d9b95e26e8de00e32c5348c254ee9e
Dominic Saladin <dominic@dsaladin.ch>
2021-04-04 17:03:39 +0200

    Fixed index out of range error with image foreground color

--- FontAwesomeWPF/FontAwesomeWPF/FontAwesomeIcon.cs
+++ FontAwesomeWPF/FontAwesomeWPF/FontAwesomeIcon.cs
@@ -8,6 +8,7 @@ namespace DSaladin.FontAwesomeWPF
     public enum FontAwesomeIcon
     {
         ///<summary>Set this value to show no icon.</summary>
+        [Description("None"), IconID("none")]
         None = 0x0,
         ///<summary>Brands 500Px</summary>
         ///<see href="https://fontawesome.com/icons/500px?style=brands"/>
