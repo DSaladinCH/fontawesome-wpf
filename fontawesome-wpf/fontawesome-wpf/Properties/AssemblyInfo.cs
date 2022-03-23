302924230795207e61bc05979f939c601c6b7219
Dominic Saladin <dominic@dsaladin.ch>
2022-03-23 08:58:30 +0100

    Added xmlns definition for nuget package

---
+++ fontawesome-wpf/fontawesome-wpf/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+using System.Runtime.InteropServices;
+using System.Windows.Markup;
+
+// In SDK-style projects such as this one, several assembly attributes that were historically
+// defined in this file are now automatically added during build and populated with
+// values defined in project properties. For details of which attributes are included
+// and how to customise this process see: https://aka.ms/assembly-info-properties
+
+
+// Setting ComVisible to false makes the types in this assembly not visible to COM
+// components.  If you need to access a type in this assembly from COM, set the ComVisible
+// attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM.
+
+[assembly: Guid("2bf22bea-8ab0-4fc0-abb5-70adb93913d8")]
+
+[assembly: XmlnsPrefix("https://dsaladin.dev/products/fontawesome/wpf/xaml", "fa")]
+[assembly: XmlnsDefinition("https://dsaladin.dev/products/fontawesome/wpf/xaml", "DSaladin.FontAwesome.WPF")]
